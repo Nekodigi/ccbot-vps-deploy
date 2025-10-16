9ae5c3123701a146d65b97fee5ff1684c0b32bbe
nekodigi <ndeji69@gmail.com>
2025-10-16 19:36:56 +0000

    Update: Firebaseを利用したPWAアプリの制作。

--- projects/e4cc059c/app.js
+++ projects/e4cc059c/app.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js';
 import { getFirestore, collection, addDoc, query, orderBy, onSnapshot } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js';
-import { getAI, getGenerativeModel, GoogleAIBackend } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-ai.js';
+import { getAI, getGenerativeModel, GoogleAIBackend } from 'https://esm.sh/firebase@11.0.2/ai';
 
 // Firebase設定
 const firebaseConfig = {
